66d4da18639ed945231fb17e87be5a9017aee8b4
Satya sai uppala <98457712+SATYASAI-UPPALA@users.noreply.github.com>
2023-06-07 08:13:37 +0530

    commit for ar project
    commit

---
+++ Assets/Scripts/LinkManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkManager : MonoBehaviour
+{
+     public void openLinkdin(){
+        Application.OpenURL("https://www.linkedin.com/in/satyasai-uppala-79801a220/");
+    }
+    public void openInstagram(){
+        Application.OpenURL("https://instagram.com/satyasaiuppala?igshid=ZDdkNTZiNTM=");
+    }
+}
